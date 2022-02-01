428032a631ad2efa39027b86ca76d91713530ecb
Robin Spielmann <robin.spielmann@virtual-identity.com>
2022-02-01 20:34:03 +0100

    initial commit 🔥

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: ["./public/**/*.html"],
+  theme: {
+    extend: {},
+    screens: {
+      'sm': '480px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    }
+  },
+  variants: {},
+  plugins: [],
+};
